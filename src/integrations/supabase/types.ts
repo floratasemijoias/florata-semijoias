55b721fa55fa40e967cb6895310f55efa634a4b9
floratasemijoias <floratasemijoiass@gmail.com>
2026-09-20 13:10:53 -0300

    Update types.ts

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -58,6 +58,7 @@ export type Database = {
           nome: string
           preco: number
           quantidade: number | null
+          subcategoria: string | null
           tamanho: string | null
         }
         Insert: {
@@ -70,6 +71,7 @@ export type Database = {
           nome: string
           preco?: number
           quantidade?: number | null
+          subcategoria?: string | null
           tamanho?: string | null
         }
         Update: {
@@ -82,6 +84,7 @@ export type Database = {
           nome?: string
           preco?: number
           quantidade?: number | null
+          subcategoria?: string | null
           tamanho?: string | null
         }
         Relationships: []
